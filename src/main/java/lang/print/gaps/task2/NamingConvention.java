eaf4c2c9d25390de02f86f4bc861858aa60f01ce
mixamtk <70889784+mixamtk@users.noreply.github.com>
2022-06-22 11:24:55 +0300

    Update NamingConvention.java

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,14 +2,23 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-       int _variable = 10;
-        int variableOne = 1;
+        int variable = 10;
+        int variable_one = 1;
         int variableTwo = 2;
-        int variableThree = 3;
+        int variable_three = 3;
         final int VARIABLE_FOUR = 4;
-        final int VARIABLE_fIVE = 5;
+        final int VARIABLE_FIVE = 5;
         int variableSix = 6;
         int variableSeven = 7;
-        int variableE_ight = 8;
+        int variableEight = 8;
+                                        line.contains("variable") ||
+                                line.contains("variable_one") ||
+                                line.contains("variableTwo") ||
+                                line.contains("variable_three") ||
+                                line.contains("VARIABLE_FOUR") ||
+                                line.contains("VARIABLE_FIVE") ||
+                                line.contains("variableSix") ||
+                                line.contains("variableSeven") ||
+                                line.contains("variableEight"))
     }
 }
